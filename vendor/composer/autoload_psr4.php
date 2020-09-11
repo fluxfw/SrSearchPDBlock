25e5ae580184a84f612ac443149634a0ec7a2761
Florian Wyss <fw@studer-raimann.ch>
2020-09-11 13:06:58 +0200

    Fix

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,10 +6,10 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'srag\\RemovePluginDataConfirm\\' => array($vendorDir . '/srag/removeplugindataconfirm/src'),
+    'srag\\RemovePluginDataConfirm\\SrSearchPDBlock\\' => array($vendorDir . '/srag/removeplugindataconfirm/src'),
     'srag\\Plugins\\SrSearchPDBlock\\' => array($baseDir . '/src'),
     'srag\\LibrariesNamespaceChanger\\' => array($vendorDir . '/srag/librariesnamespacechanger/src'),
-    'srag\\DIC\\' => array($vendorDir . '/srag/dic/src'),
-    'srag\\CustomInputGUIs\\' => array($vendorDir . '/srag/custominputguis/src'),
+    'srag\\DIC\\SrSearchPDBlock\\' => array($vendorDir . '/srag/dic/src'),
+    'srag\\CustomInputGUIs\\SrSearchPDBlock\\' => array($vendorDir . '/srag/custominputguis/src'),
     'srag\\ActiveRecordConfig\\SrSearchPDBlock\\' => array($vendorDir . '/srag/activerecordconfig/src'),
 );
